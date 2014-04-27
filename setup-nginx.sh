980451974da4e0d42385a0fad1d7e84be9a390da
Nicholas Barr <github@nicholasbarr.net>
2014-04-27 06:06:33 -0400

    nginx script: adding force yes (short) param.

--- setup-nginx.sh
+++ setup-nginx.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-sudo apt-get install python-software-properties;
-sudo apt-get install software-properties-common;
+sudo apt-get -y install python-software-properties;
+sudo apt-get -y install software-properties-common;
 sudo add-apt-repository ppa:nginx/stable;
 sudo apt-get update;
-sudo apt-get install nginx;
+sudo apt-get -y install nginx;
 
