cb246491b93adfbf1cc9a489746015c505f8d530
Nicholas Barr <github@nicholasbarr.net>
2014-04-27 15:31:48 -0400

    adding composer

--- setup-phpfpm.sh
+++ setup-phpfpm.sh
@@ -2,6 +2,9 @@
 
 sudo apt-get -y install php5-fpm php5-cli php5-mysql;
 
+# These aren't technically PHP FPM related, but they're PHP extensions.
+sudo apt-get -y install php5-curl php5-gd php5-mcrypt php-apc php-pear php5-imap php5-xmlrpc php5-sqlite;
+
 cd /etc/php5/cli;
 sudo mv php.ini php.ini.backup;
 sudo ln -s ../fpm/php.ini;
