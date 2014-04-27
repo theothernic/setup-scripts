1fa743ff68e9148b9ca536cfa07c274e7b068640
Nicholas Barr <github@nicholasbarr.net>
2014-04-27 15:32:19 -0400

    adding the needed sudo commands

--- setup-composer.sh
+++ setup-composer.sh
@@ -1,2 +1,5 @@
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+#! /usr/bin/env bash
+
+sudo curl -sS https://getcomposer.org/installer | php;
+sudo mv composer.phar /usr/local/bin/composer;
+
