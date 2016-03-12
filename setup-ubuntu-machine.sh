1151ba43c4c1a39a70e0b1f7b0b25f02e5cc1667
Nic Barr <theothernic@outlook.com>
2016-03-12 07:20:29 -0500

    Updating the node script a bit.

--- setup-ubuntu-machine.sh
+++ setup-ubuntu-machine.sh
@@ -1,13 +1,19 @@
 #! /usr/bin/env bash
 
-sudo apt-get install git; 
+#sudo apt-get install git; 
 
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -;
-sudo add-apt-repository ppa:nginx/stable;
+# Install node apt-get repos. Using the 4.x branch because
+# ver 5.x is not stable according to the developers.
+curl -sL https://deb.nodesource.com/setup_4.x | sudo bash; 
+
+# Install nginx repos.
+sudo add-apt-repository -y ppa:nginx/stable;
+
+# Update apt-get.
 sudo apt-get update;
 
 sudo apt-get install -y nodejs;
-sudo sudo apt-get --yes install nginx;
+sudo apt-get -y install nginx;
 
 sudo apt-get install mariadb-server;
 
