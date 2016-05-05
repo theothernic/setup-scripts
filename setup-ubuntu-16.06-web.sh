dad9441bf0f2a5ceae79529b46719c6ca3e71729
Nic Barr <theothernic@outlook.com>
2016-05-05 01:29:00 -0400

    autoaccept booboo

--- setup-ubuntu-16.06-web.sh
+++ setup-ubuntu-16.06-web.sh
@@ -7,8 +7,10 @@ sudo apt-get install -y build-essential;
 sudo apt-get install -y php7.0 php7.0-mysql php7.0-pgsql php7.0-gd php7.0-intl;
 sudo apt-get install -y mariadb-server;
 
-sudo add-apt-repository -y ppa:nginx/stable;
-sudo apt-get update && sudo apt-get install -y nginx;
+# sudo add-apt-repository -y ppa:nginx/stable;
+# sudo apt-get update && sudo apt-get install -y nginx
+
+sudo apt-get install -y apache2;
 
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -;
 sudo apt-get install -y nodejs;
