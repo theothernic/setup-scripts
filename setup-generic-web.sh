9d7325d29d1bce06b04b7d7e83b2886a74b4da5c
Nic Barr <theothernic@outlook.com>
2016-05-06 23:01:42 -0400

    Flesh-out of generic web server setup.

--- setup-generic-web.sh
+++ setup-generic-web.sh
@@ -9,3 +9,34 @@ else
 fi
 
 
+case $id in
+	"ubuntu")
+	echo "setting up Ubuntu-specific commands";
+	INSTALL_PKGMGR="apt-get";
+	INSTALL_PKGMGR_FORCE_FLAG="-y";
+	::
+
+	"redhat")
+	echo "setting up RH-specific commands";
+	INSTALL_PKGMGR="yum";
+	::
+
+	*)
+	echo "Your distro is not yet supported.";
+	exit 1001;
+esac
+
+
+install_system_update()
+{
+	# if this system is debian-based, make sure that apt has the latest mirrors.
+	if [ $id -eq "ubuntu" ]; then
+		sudo ${INSTALL_PKGMGR} update;
+	fi
+	
+	sudo ${INSTALL_PKGMGR} ${INSTALL_PKGMGR_FORCE_FLAG} upgrade;
+}
+
+install_system_update();
+
+
